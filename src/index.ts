ad8d98e8446df0d5693b2877ba3d871b5b126329
terasdigital <terasdigital24@gmail.com>
2026-01-12 10:28:31 +0700

    feat: add server checking endpoint

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,12 @@ async function init() {
     const app = express();
 
     const PORT = 3000;
-
     app.use(express.json());
+
+    app.get("/", (req, res) => {
+      res.status(200).json({ message: "Server is running", data: null });
+    });
+
     app.use("/api", router);
 
     app.listen(PORT, () => {
